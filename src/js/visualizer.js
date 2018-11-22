ab68f48f79455ca12c79c925f614f440d1d421c9
Vickypedialu <amylzy0204@gmail.com>
2018-11-22 19:12:07 +0800

    Set up audio processing and action handler.

--- src/js/visualizer.js
+++ src/js/visualizer.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
   visualizer = new AudioVisualizer();
   visualizer.initialize();
   visualizer.createBars();
+  visualizer.handleActions();
+  visualizer.processAudio();
 
 });
 
@@ -12,7 +14,6 @@ function AudioVisualizer(){
   this.scene;
   this.camera;
   this.renderer;
-  this.controls;
 
   // Bars are 3D shapes for music visualizations
   this.bars = [];
@@ -82,4 +83,88 @@ AudioVisualizer.prototype.createBars = function () {
     this.scene.add(this.bars[i]);
   }
 };
+
+
+// Listen to drag and drop actions, allowing users to update their own music
+AudioVisualizer.prototype.handleActions = function () {
+
+    document.body.addEventListener("dragenter", function () {
+
+    }, false);
+
+    document.body.addEventListener("dragover", function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+    }, false);
+
+    document.body.addEventListener("dragleave", function () {
+
+    }, false);
+
+    document.body.addEventListener("drop", function (e) {
+        e.stopPropagation();
+
+        e.preventDefault();
+
+        // Obtain the music file
+        let file = e.dataTransfer.files[0];
+        let fileName = file.name;
+
+        $("#guide").text("Playing " + fileName);
+
+        let fileReader = new FileReader();
+
+        fileReader.onload = function (e) {
+            let fileResult = e.target.result;
+            visualizer.start(fileResult);
+        };
+
+        fileReader.onerror = function (e) {
+            debugger
+        };
+
+        fileReader.readAsArrayBuffer(file);
+    }, false);
+};
+
+// Process music file
+AudioVisualizer.prototype.processAudio = function () {
+
+    this.audioContext = new AudioContext();
+    this.javascriptNode = this.audioContext.createScriptProcessor(2048, 1, 1);
+    this.javascriptNode.connect(this.audioContext.destination);
+
+    // Create the source buffer and analyser node
+    this.sourceBuffer = this.audioContext.createBufferSource();
+    this.analyser = this.audioContext.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.3;
+    this.analyser.fftSize = 512;
+
+    // Connect source to analyser, analyser to speaker
+    this.sourceBuffer.connect(this.analyser);
+    this.analyser.connect(this.javascriptNode);
+    this.sourceBuffer.connect(this.audioContext.destination);
+
+    let that = this;
+
+    // Animate the bars
+    this.javascriptNode.onaudioprocess = function () {
+
+        let array = new Uint8Array(that.analyser.frequencyBinCount);
+        that.analyser.getByteFrequencyData(array);
+
+        // Render the scene
+        visualizer.renderer.render(visualizer.scene, visualizer.camera);
+
+        let step = Math.round(array.length / visualizer.barNumber);
+        // Iterate through the bars and scale the z axis
+        for (let i = 0; i < visualizer.barNumber; i++) {
+            let value = array[i * step] / 4;
+            value = value < 1 ? 1 : value;
+            visualizer.bars[i].scale.z = value;
+        }
+    }
+};
+
 
